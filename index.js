584b57b93a182fb264a5463d0809b6c306720811
Begerad, Stefan <dialog@swingbe.de>
2022-05-06 11:57:43 +0200

    feat: add https for ssl

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const DEBUG=require('debug')('index');
+const HTTPS = require('https');
+const FS = require('fs');
 
 DEBUG('index start...');
 const APP=require('./src/app');
@@ -6,16 +8,22 @@ const APP=require('./src/app');
 //TODO make port available via config
 //set port
 const PORT=parseInt(process.env.PORT, 10)||65535;
-DEBUG('PORT: '+PORT)
+DEBUG('PORT: '+PORT);
 
 //TODO make env available via config
 //pass 'APP' to server
 DEBUG('NODE_ENV: '+process.env.NODE_ENV);
 if (process.env.NODE_ENV !== 'production') {
+    DEBUG('development mode');
     APP.listen(PORT);
 }else{
-    //TODO add production mode
-    DEBUG('TODO to production server');
+    DEBUG('production mode');
+    HTTPS.createServer({
+	//TODO make key and cert available via config
+        key: FS.readFileSync('./p'),
+        cert: FS.readFileSync('./f')
+    }, APP)
+    .listen(PORT, ()=>DEBUG('listening on port '+PORT));
 }
 
-DEBUG('index done..');
+DEBUG('index done.');
