ae18e1e44f47768db4086354266dc8747fb6a487
Begerad, Stefan <dialog@swingbe.de>
2022-05-18 06:17:32 +0200

    feat: adjust route trips

--- src/service/trips.js
+++ src/service/trips.js
@@ -5,7 +5,7 @@ const db = require('./db');
 
 async function getTrips(routeshortname = 0) {
     DEBUG('routeshortname: '+routeshortname);
-    const QUERY=`select agency.agency_name, routes.route_short_name, routes.agency_id, trips.route_id, trips.service_id, trips.trip_id from agency, trips, routes where agency.agency_id=routes.agency_id and trips.route_id=routes.route_id and routes.route_short_name='${routeshortname}';`
+    const QUERY=`select agency.agency_name, routes.route_short_name, routes.agency_id, trips.route_id, trips.service_id, trips.trip_id, trips.trip_short_name from agency, trips, routes where agency.agency_id=routes.agency_id and trips.route_id=routes.route_id and routes.route_short_name='${routeshortname}';`
     DEBUG('QUERY: '+QUERY);
     const TRIPS = await db.query(QUERY);
     DEBUG('TRIPS.length: '+TRIPS.length);
