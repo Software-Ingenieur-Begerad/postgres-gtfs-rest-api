da0bceaf05f703a446eac2c9b9a188b82ea5bf6f
Begerad, Stefan <dialog@swingbe.de>
2022-06-21 12:50:10 +0200

    fix: remove useless require calls

--- src/service/stops-all.js
+++ src/service/stops-all.js
@@ -2,11 +2,9 @@ const DEBUG=require('debug')('stops');
 DEBUG('stops start...');
 
 const db = require('./db');
-const helper = require('../helper');
 const config = require('../config');
 
 async function get() {
-    //const offset = helper.getOffset(page, config.listPerPage);
     const DATA = await db.query(
 	'SELECT * FROM stops');
     DEBUG('DATA length: '+DATA.length);
